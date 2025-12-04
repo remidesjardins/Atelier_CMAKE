e9c6fa3fe1aaf619824bb312f1f9754711d6a7bc
remidesjardins <118301213+remidesjardins@users.noreply.github.com>
2025-12-04 14:14:18 +0100

    Implement 'car' operation for single argument
    Added support for 'car' operation with two arguments.

--- src/main.c
+++ src/main.c
@@ -4,32 +4,45 @@
 #include "./../include/calculator.h"
 
 int main(int argc, char *argv[]){
-	if (argc == 4)
-	{
-	char* op = argv[1];
-	char* a = argv[2];
-	char* b = argv[3];
-	double r = 0;
-	if (strcmp(op, "add") == 0) {
-		r = _add(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "sub") == 0) {
-		r = _sub(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "mul") == 0) {
-		r = _mul(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "div") == 0) {
-		r = _div(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else {printf("Erreur de parametres");}
-	}
-	else {printf("Erreur de parametres");}
-	return 0;
-}
+    
+    // Cas 2 arguments : car <valeur>
+    if (argc == 3 && strcmp(argv[1], "car") == 0) {
+        char* a = argv[2];
+        double r = car(atof(a));
+        printf("%lf", r);
+        return 0;
+    }
 
-		
+    // Cas normal avec deux opérandes (add, sub, mul, div)
+    if (argc == 4)
+    {
+        char* op = argv[1];
+        char* a = argv[2];
+        char* b = argv[3];
+        double r = 0;
+
+        if (strcmp(op, "add") == 0) {
+            r = _add(atof(a), atof(b));
+        }
+        else if (strcmp(op, "sub") == 0) {
+            r = _sub(atof(a), atof(b));
+        }
+        else if (strcmp(op, "mul") == 0) {
+            r = _mul(atof(a), atof(b));
+        }
+        else if (strcmp(op, "div") == 0) {
+            r = _div(atof(a), atof(b));
+        }
+        else {
+            printf("Erreur de parametres");
+            return 1;
+        }
+
+        printf("%lf", r);
+    }
+    else {
+        printf("Erreur de parametres");
+    }
+
+    return 0;
+}
